947ac25ff12c19673fb164e9a5e74de4dfa43669
Hooman <hoomanemi2013@gmail.com>
2022-12-20 11:50:17 +0330

    add some tests

--- pages/tests.py
+++ pages/tests.py
@@ -18,3 +18,19 @@ class HomePageTest(TestCase):
     def test_home_page_template_used(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_about_us_page_url_by_name(self):
+        response = self.client.get(reverse('about_us'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_url(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_content(self):
+        response = self.client.get(reverse('about_us'))
+        self.assertContains(response, 'About us')
+
+    def test_about_us_template_used(self):
+        response = self.client.get(reverse('about_us'))
+        self.assertTemplateUsed(response, template_name='pages/about_us.html')
