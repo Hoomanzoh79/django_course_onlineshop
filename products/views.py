89092386abd19496a53f35cf670cb2f25c194caf
Hooman <hoomanemi2013@gmail.com>
2023-01-29 21:11:43 +0330

    add Comments

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
-from .models import Product
+from .models import Product, Comment
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404, render
 
 
 class ProductListView(generic.ListView):
@@ -9,7 +11,43 @@ class ProductListView(generic.ListView):
     context_object_name = 'products'
 
 
+# def product_detail_view(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     product_comments = product.comments.all()
+#     if request.method == 'POST':
+#         comment_form = CommentForm(request.POST)
+#         if comment_form.is_valid():
+#             new_comment = comment_form.save(commit=False)
+#             new_comment.product = product
+#             new_comment.author = request.user
+#             new_comment = comment_form.save()
+#             comment_form = CommentForm()
+#     else:
+#         comment_form = CommentForm()
+#     return render(request, 'products/product_detail.html', {'product': product, 'comments': product_comments,
+#                                                             'comment_form': comment_form, })
+
 class ProductDetailView(generic.DetailView):
     model = Product
-    template_name = 'products/product_detail.html'
     context_object_name = 'product'
+    template_name = 'products/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+
+class CommentCreatView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
+
+        product_id = self.kwargs['product_id']
+        product = get_object_or_404(Product, id=product_id)
+        obj.product = product
+
+        return super().form_valid(form)
